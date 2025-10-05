5f1234277e3d6612e2263a3edc783cb6df2a1870
GundapuDivyasri <divyasrigundapu@gmail.com>
2025-10-05 08:33:57 +0530

    oops

---
+++ Basics/oops.js
@@ -0,0 +1,19 @@
+//oops
+/*
+object=collection of properties and methods
+
+//parts of oops
+objcet literal
+
+-constructor function
+-prototypes
+-classes
+-instances
+*/
+const user={
+    name:"divya",
+    login:true,
+}
+for (let key in user){
+    console.log(`${key}:${user[key]}`);
+}
